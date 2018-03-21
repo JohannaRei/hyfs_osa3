0a809b6853a4d29fbdb2f014ec802d3400680f7f
Reinikainen Johanna <reinijo@Johannas-MBP.lan>
2018-03-21 18:28:44 +0200

    "ready for deployment"

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 
 app.use(bodyParser.json());
+morgan.token('data', function (req, res) { return JSON.stringify(req.body) });
+app.use(morgan(':method :url :data :status :res[content-length] - :response-time ms'));
+app.use(cors());
 
 let persons = [
   {
@@ -27,15 +32,15 @@ let persons = [
   }
 ]
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('<h1>Puhelinluettelo-API</h1>');
 });
 
-app.get('/persons', (req, res) => {
+app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
 
-app.get('/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   const person = persons.find(person => person.id === id);
   if (person) {
@@ -50,11 +55,13 @@ const generateId = () => {
   return maxId + 1;
 }
 
-app.post('/persons', (req, res) => {
+app.post('/api/persons', (req, res) => {
   const body = req.body;
 
-  if (body.name === undefined) {
+  if (body.name === undefined || body.number === undefined) {
     return res.status(400).json({ error: 'content missing' });
+  } else if (persons.filter(person => person.name === body.name).length > 0) {
+    return res.status(400).json({ error: 'name must be unique' });
   }
 
   const person = {
@@ -68,7 +75,7 @@ app.post('/persons', (req, res) => {
   res.json(person);
 });
 
-app.delete('/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter(person => person.id !== id);
   res.status(204).end();
@@ -79,7 +86,7 @@ app.get('/info', (req, res) => {
   res.send(`<p>puhelinluettelossa ${persons.length} henkilön tiedot</p><p>${date}</p>`);
 });
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () =>
   console.log(`Server running on port ${port}`)
 );
